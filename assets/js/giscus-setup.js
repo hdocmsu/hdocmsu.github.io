5456616d125a4216be17c3eb54e52f44f9ec088e
hdocmsu <hdocmsu@gmail.com>
2026-02-26 20:17:36 -0800

    changed to GISCUS

---
+++ assets/js/giscus-setup.js
@@ -0,0 +1,15 @@
+<script src="https://giscus.app/client.js"
+        data-repo="hdocmsu/hdoMRIphysics_giscus"
+        data-repo-id="R_kgDORaBXOQ"
+        data-category="hdoMRIphysics_giscus"
+        data-category-id="DIC_kwDORaBXOc4C3TnI"
+        data-mapping="pathname"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="bottom"
+        data-theme="preferred_color_scheme"
+        data-lang="en"
+        crossorigin="anonymous"
+        async>
+</script>
